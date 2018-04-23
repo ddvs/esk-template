5069b2c110209879c3655ee549cd1f6cfda8871b
AllenZho <1714080139@qq.com>
2018-04-23 10:27:56 +0800

    add v-src

--- template/plugins/inject.js
+++ template/plugins/inject.js
@@ -55,6 +55,34 @@ let directive = { // 指令
       div.style.paddingBottom = binding.value * 100 + '%'
       el.appendChild(div)
     }
+  },
+  src: {
+    inserted: function (el, binding) {
+      let pW = el.parentNode.offsetWidth
+      let pH = el.parentNode.offsetHeight
+      if (binding.modifiers.center) {
+        el.src = binding.value + '?x-oss-process=image/resize,m_fill,h_' + parseInt(pH) + ',w_' + parseInt(pW)
+        el.onload = function (e) {
+          let w = el.naturalWidth
+          let h = el.naturalHeight
+          let ratio = w / h
+          let pRatio = pW / pH
+          if (pRatio > ratio) {
+            removeClass(el, 'Hcenter')
+            addClass(el, 'Vcenter')
+            // el.classList.remove('Hcenter')
+            // el.classList.add('Vcenter')
+          } else {
+            removeClass(el, 'Vcenter')
+            addClass(el, 'Hcenter')
+            // el.classList.remove('Vcenter')
+            // el.classList.add('Hcenter')
+          }
+        }
+      } else {
+        el.src = binding.value + '?x-oss-process=image/resize,m_lfit,h_' + parseInt(pH) + ',w_' + parseInt(pW)
+      }
+    }
   }
 }
 
