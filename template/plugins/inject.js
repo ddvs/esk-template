3625c01526322b26ae3b2cd4a9a6826fb7cf8276
AllenZho <1714080139@qq.com>
2018-05-02 16:20:00 +0800

    fixbug

--- template/plugins/inject.js
+++ template/plugins/inject.js
@@ -61,34 +61,36 @@ let directive = { // 指令
   },
   src: {
     inserted: function (el, binding) {
-      let pW = el.parentNode.offsetWidth
-      let pH = el.parentNode.offsetHeight
-      if (binding.modifiers.center) {
-        let hCenter = 'Hcenter'
-        let vCenter = 'Vcenter'
-        if (binding.modifiers.full) {
-          hCenter = 'Hcenter-full'
-          vCenter = 'Vcenter-full'
-          el.src = binding.value + '?x-oss-process=image/resize,m_lfit,h_' + parseInt(pH) + ',w_' + parseInt(pW)
-        } else {
-          el.src = binding.value + '?x-oss-process=image/resize,m_fill,h_' + parseInt(pH) + ',w_' + parseInt(pW)
-        }
-        el.onload = function (e) {
-          let w = el.naturalWidth
-          let h = el.naturalHeight
-          let ratio = w / h
-          let pRatio = pW / pH
-          if (pRatio > ratio) {
-            removeClass(el, hCenter)
-            addClass(el, vCenter)
+      setTimeout(() => {
+        let pW = el.parentNode.offsetWidth
+        let pH = el.parentNode.offsetHeight
+        if (binding.modifiers.center) {
+          let hCenter = 'Hcenter'
+          let vCenter = 'Vcenter'
+          if (binding.modifiers.full) {
+            hCenter = 'Hcenter-full'
+            vCenter = 'Vcenter-full'
+            el.src = binding.value + '?x-oss-process=image/resize,m_lfit,h_' + parseInt(pH) + ',w_' + parseInt(pW)
           } else {
-            removeClass(el, vCenter)
-            addClass(el, hCenter)
+            el.src = binding.value + '?x-oss-process=image/resize,m_fill,h_' + parseInt(pH) + ',w_' + parseInt(pW)
           }
+          el.onload = function (e) {
+            let w = el.naturalWidth
+            let h = el.naturalHeight
+            let ratio = w / h
+            let pRatio = pW / pH
+            if (pRatio > ratio) {
+              removeClass(el, hCenter)
+              addClass(el, vCenter)
+            } else {
+              removeClass(el, vCenter)
+              addClass(el, hCenter)
+            }
+          }
+        } else {
+          el.src = binding.value + '?x-oss-process=image/resize,m_lfit,h_' + parseInt(pH) + ',w_' + parseInt(pW)
         }
-      } else {
-        el.src = binding.value + '?x-oss-process=image/resize,m_lfit,h_' + parseInt(pH) + ',w_' + parseInt(pW)
-      }
+      }, 10)
     }
   }
 }
